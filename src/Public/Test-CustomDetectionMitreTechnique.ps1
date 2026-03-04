11d3130d580a746e5f08acdc5f199505e14fd1c5
Fabian Bader <fabian.bader@glueckkanja.com>
2026-03-04 08:52:35 +0100

    fix: Rollback wrong optmization

--- src/Public/Test-CustomDetectionMitreTechnique.ps1
+++ src/Public/Test-CustomDetectionMitreTechnique.ps1
@@ -126,13 +126,11 @@ function Test-CustomDetectionMitreTechnique {
             }
 
             $supported = $xdrTechniqueMap[$category]
-            $supportedSet = [System.Collections.Generic.HashSet[string]]::new($supported, [System.StringComparer]::OrdinalIgnoreCase)
-
             $validTechniques = [System.Collections.Generic.List[string]]::new()
             $invalidTechniques = [System.Collections.Generic.List[string]]::new()
 
             foreach ($technique in $techniques) {
-                if ($supportedSet.Contains($technique)) {
+                if ($technique -in $supported) {
                     $validTechniques.Add($technique)
                 } else {
                     $invalidTechniques.Add($technique)
